ec4771364e92c82099aaf2299578f16bc112dc7c
Joachim Coline <joachim.coline@gmail.com>
2025-09-14 13:02:26 +0200

    fix(logging): automatic logging category not working

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <QApplication>
+#include <QDebug>
+#include <logging/Logger.h>
+#include <logging/Log.h>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    return app.exec();
+
+    JApp::Logger& logger = JApp::Logger::instance();
+    logger.initialize();
+    std::cout << QString(CURRENT_LOG_CATEGORY().categoryName()).toStdString() << std::endl;
+
+    return 0;
 }
 
