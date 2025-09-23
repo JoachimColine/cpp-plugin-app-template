44083f76fe3fc67b8bbc3c74d186e7b1de35c8ba
Joachim Coline <joachim.coline@gmail.com>
2025-09-23 19:07:16 +0200

    feat(app): style the application

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <QApplication>
+#include <QFile>
 #include <QDebug>
 #include <JApp/Logger.h>
 #include <JApp/Log.h>
 #include <JApp/Core/Gui/LogViewer.h>
 #include <JApp/Core/Models/FakeLogModel.h>
+
+QString loadStyleSheet()
+{
+    QFile styleFile(":/style.qss");
+    if (!styleFile.open(QFile::ReadOnly)) {
+        LOG_WARN() << "Failed to open stylesheet: " << styleFile.errorString();
+        return QString();
+    }
+    return QLatin1String(styleFile.readAll());
+}
 
 int main(int argc, char *argv[])
 {
@@ -11,6 +22,8 @@ int main(int argc, char *argv[])
 
     JApp::Logger& logger = JApp::Logger::instance();
     logger.initialize();
+
+    app.setStyleSheet(loadStyleSheet());
 
     JApp::Core::Gui::LogViewer* v = new JApp::Core::Gui::LogViewer(nullptr, new JApp::Core::Models::FakeLogModel());
     v->show();
