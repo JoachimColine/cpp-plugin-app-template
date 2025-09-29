d37e3a0bd6cb2f12d983ea65c248c7a0def5d60b
Joachim Coline <joachim.coline@gmail.com>
2025-09-29 16:57:40 +0200

    feat(logging): log logger initialization and shutdown

--- src/app/logging/src/Logger.cpp
+++ src/app/logging/src/Logger.cpp
@@ -74,15 +74,39 @@ void Logger::initialize(const LogConfig& config)
 
     m_initialized = true;
 
-    LOG_INFO() << QString("Logger initialized - Target: %1, Level: %2, Directory: %3")
-                  .arg(static_cast<int>(m_config.target))
-                  .arg(static_cast<int>(m_config.minLevel))
-                  .arg(m_config.logDirectory);
+    Log initLog {
+        QDateTime::currentDateTime(),
+        "Logger",
+        LogLevel::Info,
+        LogFormatter::simplifyLogFileName(__FILE__),
+        "initialize",
+        __LINE__,
+        QString("Logger initialized - Target: %1, Level: %2, Directory: %3")
+            .arg(static_cast<int>(m_config.target))
+            .arg(static_cast<int>(m_config.minLevel))
+            .arg(m_config.logDirectory),
+        QString("%1").arg(reinterpret_cast<quintptr>(QThread::currentThread()), 0, 16)
+    };
+
+    handleLog(initLog);
 }
 
 void Logger::shutdown()
 {
     if (!m_initialized) return;
+
+    Log shutdownLog {
+        QDateTime::currentDateTime(),
+        "Logger",
+        LogLevel::Info,
+        LogFormatter::simplifyLogFileName(__FILE__),
+        "shutdown",
+        __LINE__,
+        QString("Logger about to shut down, logs will be removed"),
+        QString("%1").arg(reinterpret_cast<quintptr>(QThread::currentThread()), 0, 16)
+    };
+
+    handleLog(shutdownLog);
 
     QMutexLocker locker(&m_mutex);
     
@@ -99,8 +123,10 @@ void Logger::shutdown()
         m_logFile->close();
         m_logFile.reset();
     }
-    
+
     qInstallMessageHandler(nullptr);
+
+    m_initialized = false;
 }
 
 void Logger::setLogLevel(LogLevel level)
