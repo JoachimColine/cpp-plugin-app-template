45e8324ad568c234b89bd84e4846aae0c5cded3c
Joachim Coline <joachim.coline@gmail.com>
2025-12-21 10:40:49 +0100

    feat(app): make object manager thread safe

--- src/app/core/include/JApp/Core/Managers/ObjectManager.h
+++ src/app/core/include/JApp/Core/Managers/ObjectManager.h
@@ -2,6 +2,8 @@
 #include <QSharedPointer>
 #include <QMap>
 #include <QString>
+#include <QMutex>
+#include <QMutexLocker>
 #include <JApp/Log.h>
 #include "ManagedObject.h"
 
@@ -9,6 +11,7 @@ class ObjectManager {
 public:
     template <typename T>
     void registerObject(const QString& key, QSharedPointer<T> object) {
+        QMutexLocker locker(&m_mutex);
         if (m_objects.contains(key)) {
             LOG_WARN() << QString("An object with key '%1' is already registered.").arg(key);
         } else {
@@ -18,21 +21,22 @@ public:
 
     template <typename T>
     QSharedPointer<T> getObject(const QString& key) {
+        QMutexLocker locker(&m_mutex);
         if (!m_objects.contains(key)) {
             LOG_WARN() << QString("No object found for key %1").arg(key);
         }
-
         return qSharedPointerCast<T>(m_objects.value(key, nullptr));
     }
 
     void unregisterObject(const QString& key) {
+        QMutexLocker locker(&m_mutex);
         if (!m_objects.contains(key)) {
            LOG_WARN() << QString("No object found for key %1").arg(key);
         }
-
         m_objects.remove(key);
     }
 
 private:
     QMap<QString, QSharedPointer<ManagedObject>> m_objects;
+    mutable QMutex m_mutex;
 };
